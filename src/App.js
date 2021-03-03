16b9aca3f051eddeed160e107ae96773f14fc5cc
Ming-der Wang <mingderwang@gmail.com>
2021-03-03 19:03:49 +0800

    big heading, conditional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 const Heading = tw.h1`text-blue-500 text-2xl p-2`;
-const BigHeading = tw(Heading)`text-4xl`;
+const MaybeBigHeading = styled(Heading)(({ big }) => [
+  big && tw`text-4xl`
+]);
 const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
 function App() {
   return (
     <Container>
-      <BigHeading>My big heading</BigHeading>
+      <MaybeBigHeading>My custom heading</MaybeBigHeading>
+      <MaybeBigHeading big>My big heading</MaybeBigHeading>{" "}
     </Container>
   );
 }
