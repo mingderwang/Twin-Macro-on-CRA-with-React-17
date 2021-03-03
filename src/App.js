cec5034623aa5735d8dbf05c8389d3498385fa25
Ming-der Wang <mingderwang@gmail.com>
2021-03-03 19:01:27 +0800

    add tw heading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import tw from "twin.macro";
+const Heading = tw.h1`text-blue-500 text-2xl p-2`;
+const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
 function App() {
   return (
-    <div tw="max-w-4xl mx-auto p-5 mt-5">
-      <h1 tw="text-blue-500 text-4xl">Hello world</h1>
-    </div>
+    <Container>
+      <Heading>My custom heading</Heading>
+    </Container>
   );
 }
-
 export default App;
+
