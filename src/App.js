092e6a60d0e0f60300a72504423dc4ec053c995f
Ming-der Wang <mingderwang@gmail.com>
2021-03-03 19:02:05 +0800

    big heading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import tw from "twin.macro";
 const Heading = tw.h1`text-blue-500 text-2xl p-2`;
+const BigHeading = tw(Heading)`text-4xl`;
 const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
 function App() {
   return (
     <Container>
-      <Heading>My custom heading</Heading>
+      <BigHeading>My big heading</BigHeading>
     </Container>
   );
 }
 export default App;
-
